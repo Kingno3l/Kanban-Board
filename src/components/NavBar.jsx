07827580ea920395457cbfe56138f9df6bae7437
Enoh Collins <eno.col@gmail.com>
2023-10-09 22:02:32 +0100

    Fix linters

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,19 @@
 import logo from '../images/logo.png';
 
-const NavBar = () => {
-  return (
-    <header className="header">
-      <nav className="navbar">
-        <div className="logo-container">
-          <img src={logo} alt="" className="logo" />
-          <h1>Space Travellers' Hub</h1>
-        </div>
-        <ul>
-          <li></li> //add links here
-          <li></li>
-          <li></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+const NavBar = () => (
+  <header className="header">
+    <nav className="navbar">
+      <div className="logo-container">
+        <img src={logo} alt="" className="logo" />
+        <h1>Space Travellers Hub</h1>
+      </div>
+      <ul>
+        <li />
+        <li />
+        <li />
+      </ul>
+    </nav>
+  </header>
+);
 
 export default NavBar;
