85c68dc5fab1046aede99d1ed04992a9036377bc
Enoh Collins <eno.col@gmail.com>
2023-10-10 11:46:03 +0100

    Edit app.js and included routes for the mission component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
+import Mission from './components/Mission';
 
 function App() {
   return (
     <>
       <NavBar />
+
+      <Routes>
+        <Route path="/mission" element={<Mission />} />
+      </Routes>
     </>
   );
 }
