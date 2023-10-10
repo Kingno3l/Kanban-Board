dd14facac553ee0960f910f3d15e42fd991534f4
ismail halliru muhammad <ismailhm888@gmail.com>
2023-10-10 15:28:48 +0100

    Add some files and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import Navigation from './Navigation';
+import NavRoutes from './NavRoutes';
 import './App.css';
-import NavBar from './components/NavBar';
+// import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
+      <Navigation />
+      <NavRoutes />
     </>
   );
 }
