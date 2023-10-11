70b3920dccce6c2a46c5f84174964f37885a9a49
Enoh Collins <eno.col@gmail.com>
2023-10-11 15:07:14 +0100

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navigation from './Navigation';
 import NavRoutes from './NavRoutes';
+
 import './App.css';
 
 function App() {
   return (
     <>
       <Navigation />
-
       <NavRoutes />
     </>
   );
