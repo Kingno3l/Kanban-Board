203b192f72c1b784071e2f06fdbb183c9b4572b2
Enoh Collins <eno.col@gmail.com>
2023-10-12 15:44:47 +0100

    Fix eslint errors

--- src/components/MissionProfile.jsx
+++ src/components/MissionProfile.jsx
@@ -4,8 +4,8 @@ const MissionProfile = ({ filterMission }) => (
   <div className="mission-container">
     <h1>My Missions</h1>
     {filterMission.map((m) => (
-      <ul className="mission-list">
-        <li key={m.mission_id}>{m.mission_name}</li>
+      <ul className="mission-list" key={m.mission_id}>
+        <li>{m.mission_name}</li>
       </ul>
     ))}
   </div>
