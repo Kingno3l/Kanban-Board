1eb0ef681566a9071a6398fa72971490698955ae
ismail halliru muhammad <ismailhm888@gmail.com>
2023-10-12 10:58:46 +0100

    made changes in  dragon-profile and it's contents

--- src/Links/Rocketss.js
+++ src/Links/Rocketss.js
@@ -3,13 +3,13 @@ import { useEffect } from 'react';
 import RocketsItem from './RocketItem';
 import { getDataFromServer } from '../redux/Rockets/RocketsSlice';
 
-const Rocketss = () => (
-  <div className="rocket">
-    <h1>Welcome to Home Page</h1>
-    <p>All content about the Project goes here</p>
-  </div>
-);
-export default Rocketss;
+// const Rocketss = () => (
+//   <div className="rocket">
+//     <h1>Welcome to Home Page</h1>
+//     <p>All content about the Project goes here</p>
+//   </div>
+// );
+// export default Rocketss;
 const Rockets = () => {
   const selectedData = useSelector((state) => state.rockets);
   const { loading, error, rocketData } = selectedData;
