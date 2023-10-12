4230e04e4e8092e3d8c90c12f7351e83aeac3f2c
kingno3l <nuelluiz@gmail.com>
2023-10-12 10:42:19 +0100

    profile fetching

--- src/Links.js/Profile.js
+++ src/Links.js/Profile.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <div className="profile">
-    <h2>This is a Profile page</h2>
-    <p>All content about the Profile goes here</p>
-  </div>
-);
-export default Profile;
+const MyProfile = () => {
+  const { rocketData } = useSelector((state) => state.rockets);
+  const filterRockets = rocketData.filter((rocket) => rocket.reserved);
+  return (
+    <>
+      <div className="profile">
+        <div className="">
+          <h2 className="">My Rockets</h2>
+          <ul className="">
+            {filterRockets.map((rocket) => (
+              <li key={rocket.id} className="">
+                {rocket.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MyProfile;
