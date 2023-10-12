92254e30d2e8027fb42bf2f0cb64823244e75111
kingno3l <nuelluiz@gmail.com>
2023-10-12 10:34:08 +0100

    reserved implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Navigation from './Navigation';
 import NavRoutes from './NavRoutes';
+import { getDataFromServer } from './redux/Rockets/RocketsSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDataFromServer());
+  }, [dispatch]);
   return (
     <>
       <Navigation />
--- src/redux/Rockets/RocketsSlice.js
+++ src/redux/Rockets/RocketsSlice.js
@@ -1,13 +1,13 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 
 const baseUrl = 'https://api.spacexdata.com/v4/rockets';
 export const getDataFromServer = createAsyncThunk(
   'Rockets/getDataFromServer',
   async () => {
     try {
-      const response = await axios.get(baseUrl);
-      return response.data;
+      const response = await fetch(baseUrl);
+      const data = await response.json();
+      return data;
     } catch (error) {
       return error.message;
     }
@@ -23,11 +23,10 @@ const RocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {
-    reserved: (state, action) => {
+    reserveRocket: (state, action) => {
       const rocket = state.rocketData.find(
         (rocket) => rocket.id === action.payload,
       );
-      // rocket.reserved = rocket.reserved ? false : true;
       rocket.reserved = !rocket.reserved;
     },
   },
@@ -43,7 +42,6 @@ const RocketsSlice = createSlice({
           image: rocket.flickr_images[0],
           name: rocket.name,
           description: rocket.description,
-          type: rocket.type,
         }));
       })
       .addCase(getDataFromServer.rejected, (state, action) => {
@@ -54,4 +52,4 @@ const RocketsSlice = createSlice({
 });
 
 export default RocketsSlice.reducer;
-export const { reserved, cancelled } = RocketsSlice.actions;
+export const { reserveRocket } = RocketsSlice.actions;
