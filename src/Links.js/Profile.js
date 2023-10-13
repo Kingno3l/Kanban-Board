a9e51cb32d944cb5a1e359a35954eda3b55c5893
Enoh Collins <eno.col@gmail.com>
2023-10-13 06:43:07 +0100

    Fix errors

--- src/Links.js/Profile.js
+++ src/Links.js/Profile.js
@@ -9,26 +9,26 @@ const MyProfile = () => {
   const filterRockets = rocketData.filter((rocket) => rocket.reserved);
   const filterMission = mission.filter((m) => m.status === 'Active member');
   return (
-    <>
-      <ReservedDragons />
-      <section className="profile-section">
-        <div className="profile">
-          <div className="rocket-card">
-            <h2 className="title-p">My Rockets</h2>
-            <ul className="profile-card">
-              {filterRockets.map((rocket) => (
-                <li key={rocket.id} className="ul-list-item">
-                  {rocket.name}
-                </li>
-              ))}
-            </ul>
-          </div>
+    <section className="profile-section">
+      <div>
+        <ReservedDragons />
+      </div>
+      <div className="profile">
+        <div className="rocket-card">
+          <h2 className="title-p">My Rockets</h2>
+          <ul className="profile-card">
+            {filterRockets.map((rocket) => (
+              <li key={rocket.id} className="ul-list-item">
+                {rocket.name}
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="mission-profile">
-          <MissionProfile filterMission={filterMission} />
-        </div>
-      </section>
-    </>
+      </div>
+      <div className="mission-profile">
+        <MissionProfile filterMission={filterMission} />
+      </div>
+    </section>
   );
 };
 
