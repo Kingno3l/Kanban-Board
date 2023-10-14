7ec7bf0a67d0d3ea6369ed20908b92a9d1eccc6e
kingno3l <nuelluiz@gmail.com>
2023-10-14 20:20:00 +0100

    fixed linter

--- src/__test__/Dragons.test.js
+++ src/__test__/Dragons.test.js
@@ -1,19 +1,18 @@
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import renderer from "react-test-renderer";
-import Dragon from "../components/Dragon";
-import store from "../redux/store";
-import Dragons from "../routes/Dragons";
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Dragon from '../components/Dragon';
+import store from '../redux/store';
 
-describe("Dragons", () => {
-  test("Render Dragons Component", () => {
+describe('Dragons', () => {
+  test('Render Dragons Component', () => {
     const tree = renderer
       .create(
         <Provider store={store}>
           <BrowserRouter>
             <Dragon />
           </BrowserRouter>
-        </Provider>
+        </Provider>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
